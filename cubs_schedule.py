c3df082e44a92b6ba3d208dc29a1bf74cfd5c8f7
Brandon Gray <graybrandon@gmail.com>
2020-03-13 06:30:11 -0500

    Initial code commit

---
+++ cubs_schedule.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import statsapi
+import datetime
+from dateutil import tz
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import os
+
+sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+destinations = os.environ.get("DESTINATIONS")
+from_email = os.environ.get("FROM_EMAIL")
+
+team = statsapi.lookup_team('chc')[0]
+today = datetime.date.today().strftime("%m/%d/%Y")
+
+games = statsapi.schedule(team=team["id"], date=today)
+
+from_zone = tz.tzutc()
+to_zone = tz.gettz("America/Chicago")
+
+for game in games:
+    dt = datetime.datetime.strptime(game["game_datetime"], "%Y-%m-%dT%H:%M:%SZ")
+    time = dt.replace(tzinfo=from_zone).astimezone(to_zone).strftime('%I:%M %p')
+    message = "{} @ {}".format(game["summary"], time)
+    
+    email_message = Mail(
+        from_email=from_email,
+        to_emails=destinations,
+        subject="MLB",
+        html_content=message
+    )
+
+    response = sg.send(email_message)
